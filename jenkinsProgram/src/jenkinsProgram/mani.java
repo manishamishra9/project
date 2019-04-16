d1ccf9c0a8c52229e19c885feb7d60259756c06e
manishamishra9 <49015833+manishamishra9@users.noreply.github.com>
2019-04-17 00:09:39 +0530

    zc

---
+++ jenkinsProgram/src/jenkinsProgram/mani.java
@@ -0,0 +1,16 @@
+package jenkinsProgram;
+
+public abstract class  mani {
+
+	
+	public static  final int maa=9;
+	abstract void manish();
+	public void manu() {
+		System.out.println("i m fool");
+	}
+         
+	public static void main(String[] args) {
+		mani m= new mani();
+	}
+
+}
