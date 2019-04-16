79768e41254afeb030e0413b7176729e355bb7a4
manishamishra9 <49015833+manishamishra9@users.noreply.github.com>
2019-04-17 00:09:07 +0530

    changes

---
+++ jenkinsProgram/src/jenkinsProgram/mukesh.java
@@ -0,0 +1,22 @@
+package jenkinsProgram;
+
+public class mukesh extends mani{
+	
+    //private int maa;
+        
+	 public void manish() {
+		 
+		System.out.println("i m great");
+	 }
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            mukesh m= new mukesh();
+            m.manu();
+            System.out.println(m.maa);
+            m.manish();
+            
+	}
+
+}
